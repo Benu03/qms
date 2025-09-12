8bef0a2b6dab5d4a1acad3f04139df67658eb7d4
Your Name <ibnu.khoirin03@gmail.com>
2025-09-12 16:18:31 +0700

    ya

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
-    DashboardController,
-    MainController,
-    MasterController,
-    SecurityController,
-    CleanController,
-    ReportController
+    MainController
 
 };
 
@@ -22,33 +17,8 @@ use App\Http\Controllers\{
 |
 */
 
-Route::group(['middleware' => ['session_key']],function(){
+Route::get('/', [MainController::class, 'index'])->name('index');
 
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-    Route::get('/profile', [MainController::class, 'profile'])->name('profile');
-    Route::post('/logout', [MainController::class, 'logout'])->name('logout');
-    Route::get('/lobby', [MainController::class, 'lobby'])->name('lobby');
-    Route::get('/get-notification', [MainController::class, 'getNotifications'])->name('getnotif');
-    Route::post('/update-notif', [MainController::class, 'updateNotifIsread'])->name('updatenotif');
-
-
-    Route::get('/master-data', [MasterController::class, 'masterData'])->name('master-data');
-    Route::get('/karyawan', [MasterController::class, 'karyawan'])->name('karyawan');
-    Route::get('/perusahaan', [MasterController::class, 'perusahaan'])->name('perusahaan');
-    Route::get('/presensi', [MasterController::class, 'presensi'])->name('presensi');
-
-
-    
-    Route::get('/security', [SecurityController::class, 'Security'])->name('security');
-    
-    
-    Route::get('/cleaning', [CleanController::class, 'Cleaning'])->name('cleaning');
-    
-    
-    Route::get('/report', [ReportController::class, 'report'])->name('report');
-    
-});
 
 Route::fallback(function () {
     return response()->view('global.notification.url_forbidden', ['title' => 'Forbidden'], 403);
